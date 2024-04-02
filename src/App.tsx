50e7336ba14530e5ef65a9c72a0c341ed5c54f0b
Nabeel Siddiqui <nabeelksiddiqui@gmail.com>
2024-04-02 15:10:33 -0400

    Add files via upload
    welcome page update

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './pages/Welcome';
+
+function App() {
+  return <Welcome />;
+}
+
+export default App;
